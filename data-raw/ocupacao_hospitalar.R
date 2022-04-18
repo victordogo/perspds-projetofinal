9974be2a3b03e4146d9632b1d41a98e676c4df61
Victor Dogo <victor.dogo@predify.me>
2022-04-17 21:40:51 -0300

    adicao de comentarios e upload do app em .Rmd

--- data-raw/ocupacao_hospitalar.R
+++ data-raw/ocupacao_hospitalar.R
@@ -2,11 +2,15 @@
 
 library(tidyverse)
 
+# Lendo banco de dados do datasus
+
 ocupacao_hospitalar <- read_csv('https://s3.sa-east-1.amazonaws.com/ckan.saude.gov.br/LEITOS/2022-04-15/esus-vepi.LeitoOcupacao_2021.csv')
 
+# Tratando banco de dados
+
 ocupacao_hospitalar <- ocupacao_hospitalar |>
-  filter(municipio %in% c("São Paulo", "Rio de Janeiro", "Belo Horizonte")) |>
-  mutate(data=lubridate::date(dataNotificacao)) |>
+  filter(municipio %in% c("São Paulo", "Rio de Janeiro", "Belo Horizonte")) |> # selecionando sao paulo, rio de janeiro e bh
+  mutate(data=lubridate::date(dataNotificacao)) |> # extraindo data
   group_by(data,municipio) |>
   distinct(data,municipio, .keep_all = TRUE) |>
   summarise(
@@ -14,9 +18,13 @@ ocupacao_hospitalar <- ocupacao_hospitalar |>
       .cols=ocupacaoSuspeitoCli:saidaConfirmadaAltas,
       .fns=~sum(.x, na.rm=TRUE)
     )
-  )
+  ) # somando ocorrencias de todos os hospitais das tres cidades
+
+# Exportando banco de dados
 
 readr::write_rds(ocupacao_hospitalar, 'data/ocupacao_hospitalar.rds')
+
+# Criando banco de dados acumulados
 
 ocupacao_hospitalar_cumsum <- ocupacao_hospitalar |>
   group_by(municipio) |>
@@ -26,6 +34,8 @@ ocupacao_hospitalar_cumsum <- ocupacao_hospitalar |>
       .fns=~cumsum(.x)
     )
   )
+
+# Exportando banco de dados
 
 readr::write_rds(ocupacao_hospitalar_cumsum,
                  'data/ocupacao_hospitalar_cumsum.rds')
